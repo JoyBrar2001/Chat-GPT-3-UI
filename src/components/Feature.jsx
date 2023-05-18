e35de18d8931d58d05ebd20adeb955b1acf00882
Joy Brar <joybrar20001@gmail.com>
2023-05-18 10:13:44 +0530

    Added GPT-3 Section

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>
-      
+    <div className={`w-full flex ${title === 'What is GPT-3 ?' ? 'flex-col md:flex-row justify-between' : 'flex-col'} gap-4  items-start m-4`}>
+      <div className='max-w-[180px] mr-8 relative'>
+        <div className='absolute w-[38px] h-[3px] feature__gradient top-[-10px]' />
+        <h1 className='text-white text-xl font-semibold'>{title}</h1>
+      </div>
+      <div className='flex max-w-[390px]'>
+        <p className='text-[#81AFDD]'>
+          {text}
+        </p>
+      </div>
     </div>
   )
 }
--- src/components/WhatGPT3.jsx
+++ src/components/WhatGPT3.jsx
@@ -3,19 +3,19 @@ import Feature from './Feature'
 
 const WhatGPT3 = () => {
   return (
-    <div className='gpt3__whatgpt3 section__margin mx-24 my-12' id="whpt3">
-      <div className='flex'>
-        <Feature />
+    <div className='gpt3__whatgpt3 section__margin section__padding pt-12' id="whpt3">
+      <div className='flex flex-col w-full'>
+        <Feature title='What is GPT-3 ?' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' />
         
-        <div>
-            <h1 className='gradient__text'>Possibilities are beyond your imagination</h1>
-            <p>Explore The Library</p>
+        <div className='flex flex-row gap-16 justify-between items-center w-full mt-16 mb-8'>
+            <h1 className='gradient__text font-bold text-4xl'>Possibilities are beyond your imagination</h1>
+            <p className='text-lg gradient__text font-bold cursor-pointer'>Explore The Library</p>
         </div>
 
-        <div>
-            <Feature />
-            <Feature />
-            <Feature />
+        <div className='flex flex-col md:flex-row mt-8'>
+            <Feature title='Chatbots' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought'/>
+            <Feature title='Knowledgebase' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b' />
+            <Feature title='Education' text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b' />
         </div>
       </div>
     </div>
